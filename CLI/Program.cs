ce6698fbe593ff0d3deafad0fa8396a5ea79fc15
OctavianNaom <octaviannaom@gmail.com>
2024-09-14 17:11:09 +0200

    Frame

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting CLI app...");
+IUserRepository userRepository= new UserMemoryRepository();
+ICommentRepository commentRepository= new CommentMemoryRepository();
+IPostRepository postRepository = new PostMemoryRepository();
+
+CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
+await cliApp.StartAsync();
