4db02aeeab4f5510b9e35108133619d597079f0f
OctavianNaom <octaviannaom@gmail.com>
2024-09-14 18:47:05 +0200

    CliApp and Program

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace CLI.UI;
+﻿// See https://aka.ms/new-console-template for more information
+namespace CLI.UI;
 using InMemoryRepositories;
 using RepositoryContracts;
 
-Console.WriteLine("Starting CLI app...");
-IUserRepository userRepository= new UserMemoryRepository();
-ICommentRepository commentRepository= new CommentMemoryRepository();
-IPostRepository postRepository = new PostMemoryRepository();
-
-CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
-await cliApp.StartAsync();
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IUserRepository userRepository = new UserMemoryRepository();
+        ICommentRepository commentRepository = new CommentMemoryRepository();
+        IPostRepository postRepository = new PostMemoryRepository();
+        
+        CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
+        Console.WriteLine("Welcome to the CLI App!");
+        await cliApp.StartAsync();
+    }
+}
