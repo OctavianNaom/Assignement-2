6b6c2c2cece8a9e3000cf369f59603901427ff04
OctavianNaom <61383527+OctavianNaom@users.noreply.github.com>
2024-10-24 12:50:31 +0200

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<GlobalExceptionHandlerMiddleware>();
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,7 +18,7 @@ var app = builder.Build();
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 app.MapControllers();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
